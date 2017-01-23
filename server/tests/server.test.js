c0fc394dbc004bbeea5a59cdb7f28f9e07e869e8
Asher Genach <asher.genach@gmail.com>
2017-01-23 02:22:42 +0200

    Added tests for GET/todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -6,8 +6,19 @@ const {Todo} = require('./../models/todo');
 
 // Remove all DB data before running the test
 // done as it is Async
+var todos = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+
+// Remove all
+// Then insert the above array => So we will have something for GET/todos.
+// Then done.
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -28,7 +39,7 @@ describe('POST /todos', () => {
        }
 
        // Search in DB
-       Todo.find().then((todos) => {
+       Todo.find({text}).then((todos) => { // We search only for text because we want the result to still be 1
          expect(todos.length).toBe(1);
          expect(todos[0].text).toBe(text);
          done();
@@ -52,10 +63,22 @@ describe('POST /todos', () => {
 
        // Search in DB
        Todo.find().then((todos) => {
-         expect(todos.length).toBe(0); // No data in the database since the post failed.
+         expect(todos.length).toBe(2); // Only the two initial items are in the database data since the post failed.
          done();
        }).catch((e) => done(e))
      });
+  });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+          expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
 
   });
 });
